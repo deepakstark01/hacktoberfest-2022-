6fdb179fa88709ea19b15204dac08dc111728c8b
raghavgh <paliwal.raghav9352@gmail.com>
2022-10-09 18:08:00 +0530

    added code to swap two numbers without using extra memory

--- Golang/swap.go
+++ Golang/swap.go
@@ -5,17 +5,21 @@ import "fmt"
 func main() {
 	var num1 int = 11
 	var num2 int = 22
-	var num3 int = 0
 
 	fmt.Println("Numbers before swapping:")
 	fmt.Println("Num1: ", num1)
 	fmt.Println("Num2: ", num2)
 
-	num3 = num1
-	num1 = num2
-	num2 = num3
+	swap(&num1, &num2)
 
 	fmt.Println("Numbers after swapping:")
 	fmt.Println("Num1: ", num1)
 	fmt.Println("Num2: ", num2)
+}
+
+// swap takes two pointers of numbers and swap those by arithmetic operations
+func swap(num1, num2 *int) {
+	*num1 += *num2
+	*num2 = *num1 - *num2
+	*num1 -= *num2
 }
